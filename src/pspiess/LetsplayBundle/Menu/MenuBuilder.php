d6d701895012f02d93f51e0ade998ce23528289c
pspiess <info@pspiess.de>
2014-08-07 00:08:24 +0200

    - Implementation of a Basic Menu with bootsrap

---
+++ src/pspiess/LetsplayBundle/Menu/MenuBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace pspiess\LetsplayBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class MenuBuilder extends ContainerAware {
+
+    public function mainMenu(FactoryInterface $factory, array $options) {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav');
+
+        $menu->addChild('Projects', array('route' => 'pspiess_letsplay_projects'))
+                ->setAttribute('icon', 'icon-list');
+
+        $menu->addChild('Employees', array('route' => 'pspiess_letsplay_employees'))
+                ->setAttribute('icon', 'icon-group');
+
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory, array $options) {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav pull-right');
+
+        /*
+          You probably want to show user specific information such as the username here. That's possible! Use any of the below methods to do this.
+
+          if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+          $username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+
+         */
+        $menu->addChild('User', array('label' => 'Hi visitor'))
+                ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'icon-user');
+
+        $menu['User']->addChild('Edit profile', array('route' => 'pspiess_letsplay_profile'))
+                ->setAttribute('icon', 'icon-edit');
+
+        return $menu;
+    }
+
+}
