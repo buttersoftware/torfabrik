073fe2cc4ccee8d1cb513e2f866f8a443cb4f786
pspiess <info@pspiess.de>
2015-07-16 23:24:27 +0200

    - 2 new Fields in from

--- src/pspiess/LetsplayBundle/Form/CustomerType.php
+++ src/pspiess/LetsplayBundle/Form/CustomerType.php
@@ -29,6 +29,8 @@ class CustomerType extends AbstractType {
                 ->add('phone', 'text', array('label' => 'Telefon', 'attr' => array('class' => '')))
                 ->add('mobile', 'text', array('label' => 'Mobil', 'attr' => array('class' => ''), 'required' => false))
                 ->add('fax', 'text', array('label' => 'Fax', 'attr' => array('class' => ''), 'required' => false))
+                ->add('contactPerson', 'text', array('label' => 'Ansprechpartner', 'attr' => array('class' => '', 'required' => false)))
+                ->add('contactPersonPhone', 'text', array('label' => 'Telefon', 'attr' => array('class' => ''), 'required' => false))
                 ->add('note', 'text', array('label' => 'Bemerkung', 'attr' => array('class' => ''), 'required' => false))
                 ->add('discount', 'text', array('label' => 'Rabatt', 'attr' => array('class' => ''), 'required' => false))
                 ->add('birthday', 'date', array('years' => range(1950, 2005), 'empty_value' => array('year' => 'Jahr', 'month' => 'Monat', 'day' => 'Tag'), 
